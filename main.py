c4ef627a940b0801e2fac6f638daff5c113bf556
safwanahendouz <safwanahendouz241@gmail.com>
2026-04-25 21:36:54 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from tkinter import *
+import tkinter as tk
+from geopy.geocoders import Nominatim
+from tkinter import ttk,messagebox
+from timezonefinder import TimezoneFinder
+from datetime import datetime
+import requests
+import pytz
+
+root=Tk()
+root.title("Weather App")
+root.geometry("900x500+300+200")
+root.resizable(False,False)
+
+#Search box
+Search_image=PhotoImage(file="search.png")
+myimage=Label(image=Search_image)
+myimage.place(x=20,y=20)
+
+textfield=tk.Entry(root,justify="center",width=17,font=("poppins",25,"bold"),bg="#404040",border=0,fg="white")
+textfield.place(x=50,y=40)
+textfield.focus()
+
+
+
