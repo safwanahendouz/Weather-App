5d4dda09c095fadaeeb609f750e2df9517554812
safwanahendouz <safwanahendouz241@gmail.com>
2026-04-26 16:15:25 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -48,6 +48,35 @@ label3.place(x=430,y=400)
 label4=Label(root,text="PRESSURE",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
 label4.place(x=650,y=400)
 
+def getWeather():
+    city=textfield.get()
+    geolocator=Nominatim(user_agent="geoapiExercises")
+    location=geolocator.geocode(city)
+    obj=TimezoneFinder()
+    result=obj.timezone_at(lng=location.longitude,lat=location.latitude)
+
+    home=pytz.timezone(result)
+    local_time=datetime.now(home)
+    current_time=local_time.strftime("%I:%M %p")
+    clock.config(text=current_time)
+    name.config(text="CURRENT WEATHER")
+
+    api="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=YOUR_API_KEY"
+    json_data=requests.get(api).json()
+    condition=json_data['weather'][0]['main']
+    description=json_data['weather'][0]['description']
+    temp=int(json_data['main']['temp']-273.15)
+    pressure=json_data['main']['pressure']
+    humidity=json_data['main']['humidity']
+    wind=json_data['wind']['speed']
+
+    t.config(text=(temp,"°C"))
+    c.config(text=(condition,"|","FEELS","LIKE",temp,"°C"))
+    w.config(text=wind)
+    h.config(text=humidity)
+    d.config(text=description)
+    p.config(text=pressure)
+
 
 
 
